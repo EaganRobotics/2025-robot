5e3d037f003c555c6b13739c59f408b638c28d3c
notGriffin <104639986+scuyul@users.noreply.github.com>
2025-02-28 21:22:27 -0600

    fixed snap to source

--- src/main/java/frc/robot/Robot25/commands/DriveCommands.java
+++ src/main/java/frc/robot/Robot25/commands/DriveCommands.java
@@ -75,16 +75,16 @@ public class DriveCommands {
   private static final double Right_Loading_Station_X = 43.3071;
   private static final double Right_Loading_Station_Y = 29.52756;
 
-  private static final Translation2d Left_Loading_Station =
-      new Translation2d(Inches.of(Left_Loading_Station_X), Inches.of(Left_Loading_Station_Y));
+  private static final Translation2d Left_Loading_Station = new Translation2d(Inches.of(Left_Loading_Station_X),
+      Inches.of(Left_Loading_Station_Y));
 
-  private static final Translation2d Right_Loading_Station =
-      new Translation2d(Inches.of(Right_Loading_Station_X), Inches.of(Right_Loading_Station_Y));
+  private static final Translation2d Right_Loading_Station = new Translation2d(Inches.of(Right_Loading_Station_X),
+      Inches.of(Right_Loading_Station_Y));
 
-  private static final Translation2d REEF_CENTER =
-      new Translation2d(Inches.of(REEF_CENTER_X_INCHES), Inches.of(REEF_CENTER_Y_INCHES));
-  private static final Transform2d REEF_BRANCH_TO_ROBOT =
-      new Transform2d(Inches.of(-INCHES_FROM_REEF), Inches.zero(), Rotation2d.kZero);
+  private static final Translation2d REEF_CENTER = new Translation2d(Inches.of(REEF_CENTER_X_INCHES),
+      Inches.of(REEF_CENTER_Y_INCHES));
+  private static final Transform2d REEF_BRANCH_TO_ROBOT = new Transform2d(Inches.of(-INCHES_FROM_REEF), Inches.zero(),
+      Rotation2d.kZero);
 
   private static final Pose2d[] REEF_POSITIONS = new Pose2d[] {
       new Pose2d(REEF_CENTER.plus(new Translation2d(Inches.of(-20.738000), Inches.of(6.482000))),
@@ -113,10 +113,9 @@ public class DriveCommands {
           Rotation2d.fromDegrees(300)).transformBy(REEF_BRANCH_TO_ROBOT),
 
       new Pose2d(Left_Loading_Station.plus(new Translation2d(Inches.of(0), Inches.of(0))),
-          Rotation2d.fromDegrees(-50 + 180 + 180)).transformBy(REEF_BRANCH_TO_ROBOT),
+          Rotation2d.fromDegrees(-50 + 180 + 180)),
       new Pose2d(Right_Loading_Station.plus(new Translation2d(Inches.of(0), Inches.of(0))),
-          Rotation2d.fromDegrees(130 + 90 + 180)).transformBy(REEF_BRANCH_TO_ROBOT),};
-
+          Rotation2d.fromDegrees(130 + 90 + 180)) };
 
   // public static final TunableDouble ANGLE_KP =/
   // new TunableDouble("ANGLE_KP", 7.0, "driver").setSpot(0, 0);
@@ -125,22 +124,16 @@ public class DriveCommands {
   // public static final TunableDouble ANGLE_KD =
   // new TunableDouble("ANGLE_KD", 0.4, "driver").setSpot(2, 0);
 
-  public static final LoggedNetworkNumber ANGLE_KP =
-      new LoggedNetworkNumber("/Tuning/angleKP", 7.0);
-  public static final LoggedNetworkNumber ANGLE_KI =
-      new LoggedNetworkNumber("/Tuning/angleKI", 0.0);
-  public static final LoggedNetworkNumber ANGLE_KD =
-      new LoggedNetworkNumber("/Tuning/angleKD", 0.4);
+  public static final LoggedNetworkNumber ANGLE_KP = new LoggedNetworkNumber("/Tuning/angleKP", 7.0);
+  public static final LoggedNetworkNumber ANGLE_KI = new LoggedNetworkNumber("/Tuning/angleKI", 0.0);
+  public static final LoggedNetworkNumber ANGLE_KD = new LoggedNetworkNumber("/Tuning/angleKD", 0.4);
 
-  public static final LoggedNetworkNumber POSITION_KP =
-      new LoggedNetworkNumber("/Tuning/positionKP", 4);
-  public static final LoggedNetworkNumber POSITION_KI =
-      new LoggedNetworkNumber("/Tuning/positionKI", 0); // 1
-  public static final LoggedNetworkNumber POSITION_KD =
-      new LoggedNetworkNumber("/Tuning/positionKD", 0); // 1
+  public static final LoggedNetworkNumber POSITION_KP = new LoggedNetworkNumber("/Tuning/positionKP", 4);
+  public static final LoggedNetworkNumber POSITION_KI = new LoggedNetworkNumber("/Tuning/positionKI", 0); // 1
+  public static final LoggedNetworkNumber POSITION_KD = new LoggedNetworkNumber("/Tuning/positionKD", 0); // 1
 
-
-  private DriveCommands() {}
+  private DriveCommands() {
+  }
 
   private static Translation2d getLinearVelocityFromJoysticks(double x, double y) {
     // Apply deadband
@@ -156,15 +149,15 @@ public class DriveCommands {
   }
 
   /**
-   * Field relative drive command using two joysticks (controlling linear and angular velocities).
+   * Field relative drive command using two joysticks (controlling linear and
+   * angular velocities).
    */
   public static Command joystickDrive(Drive drive, DoubleSupplier xSupplier,
       DoubleSupplier ySupplier, DoubleSupplier omegaSupplier) {
 
     // Create PID controller
-    ProfiledPIDController angleController =
-        new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
-            new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
+    ProfiledPIDController angleController = new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
+        new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
     angleController.enableContinuousInput(-Math.PI, Math.PI);
     angleController.setTolerance(ANGLE_TOLERANCE);
 
@@ -174,8 +167,7 @@ public class DriveCommands {
       angleController.setD(ANGLE_KD.get());
 
       // Get linear velocity
-      Translation2d linearVelocity =
-          getLinearVelocityFromJoysticks(-xSupplier.getAsDouble(), -ySupplier.getAsDouble());
+      Translation2d linearVelocity = getLinearVelocityFromJoysticks(-xSupplier.getAsDouble(), -ySupplier.getAsDouble());
 
       // Apply rotation deadband
       double omega = MathUtil.applyDeadband(omegaSupplier.getAsDouble(), DEADBAND);
@@ -216,33 +208,32 @@ public class DriveCommands {
   }
 
   /**
-   * Field relative drive command using joystick for linear control and PID for angular control.
-   * Possible use cases include snapping to an angle, aiming at a vision target, or controlling
+   * Field relative drive command using joystick for linear control and PID for
+   * angular control.
+   * Possible use cases include snapping to an angle, aiming at a vision target,
+   * or controlling
    * absolute rotation with a joystick.
    */
   public static Command joystickDriveAtAngle(Drive drive, DoubleSupplier xSupplier,
       DoubleSupplier ySupplier, Supplier<Rotation2d> rotationSupplier) {
 
     // Create PID controller
-    ProfiledPIDController angleController =
-        new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
-            new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
+    ProfiledPIDController angleController = new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
+        new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
     angleController.enableContinuousInput(-Math.PI, Math.PI);
 
     // Construct command
     return Commands.run(() -> {
       // Get linear velocity
-      Translation2d linearVelocity =
-          getLinearVelocityFromJoysticks(xSupplier.getAsDouble(), ySupplier.getAsDouble());
+      Translation2d linearVelocity = getLinearVelocityFromJoysticks(xSupplier.getAsDouble(), ySupplier.getAsDouble());
 
       // Calculate angular speed
       double omega = angleController.calculate(drive.getRotation().getRadians(),
           rotationSupplier.get().getRadians());
 
       // Convert to field relative speeds & send command
-      ChassisSpeeds speeds =
-          new ChassisSpeeds(linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(),
-              linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(), omega);
+      ChassisSpeeds speeds = new ChassisSpeeds(linearVelocity.getX() * drive.getMaxLinearSpeedMetersPerSec(),
+          linearVelocity.getY() * drive.getMaxLinearSpeedMetersPerSec(), omega);
       drive.runVelocity(ChassisSpeeds.fromFieldRelativeSpeeds(speeds, drive.getRotation()));
     }, drive)
 
@@ -296,8 +287,6 @@ public class DriveCommands {
 
     return desiredPose;
   };
-
-
 
   /**
    * Measures the velocity feedforward constants for the drive motors.
@@ -401,8 +390,7 @@ public class DriveCommands {
                   for (int i = 0; i < 4; i++) {
                     wheelDelta += Math.abs(positions[i] - state.positions[i]) / 4.0;
                   }
-                  double wheelRadius =
-                      (state.gyroDelta * DriveConstants.DRIVE_BASE_RADIUS) / wheelDelta;
+                  double wheelRadius = (state.gyroDelta * DriveConstants.DRIVE_BASE_RADIUS) / wheelDelta;
 
                   NumberFormat formatter = new DecimalFormat("#0.000");
                   System.out.println("********** Wheel Radius Characterization Results **********");
@@ -424,20 +412,19 @@ public class DriveCommands {
   public static Command snapToPosition(Drive drive, Pose2d desiredPosition) {
 
     // Create PID controller
-    ProfiledPIDController angleController =
-        new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
-            new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
+    ProfiledPIDController angleController = new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
+        new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
     angleController.enableContinuousInput(-Math.PI, Math.PI);
     angleController.setTolerance(ANGLE_TOLERANCE);
 
-    ProfiledPIDController xController =
-        new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(), POSITION_KD.get(),
-            new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
+    ProfiledPIDController xController = new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(),
+        POSITION_KD.get(),
+        new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
     xController.setTolerance(POSITION_TOLERANCE);
 
-    ProfiledPIDController yController =
-        new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(), POSITION_KD.get(),
-            new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
+    ProfiledPIDController yController = new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(),
+        POSITION_KD.get(),
+        new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
     yController.setTolerance(POSITION_TOLERANCE);
 
     return Commands.run(() -> {
@@ -488,20 +475,19 @@ public class DriveCommands {
       DoubleSupplier ySupplier, DoubleSupplier omegaSupplier, BooleanSupplier snapSupplier) {
 
     // Create PID controller
-    ProfiledPIDController angleController =
-        new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
-            new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
+    ProfiledPIDController angleController = new ProfiledPIDController(ANGLE_KP.get(), ANGLE_KI.get(), ANGLE_KD.get(),
+        new TrapezoidProfile.Constraints(ANGLE_MAX_VELOCITY, ANGLE_MAX_ACCELERATION));
     angleController.enableContinuousInput(-Math.PI, Math.PI);
     angleController.setTolerance(ANGLE_TOLERANCE);
 
-    ProfiledPIDController xController =
-        new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(), POSITION_KD.get(),
-            new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
+    ProfiledPIDController xController = new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(),
+        POSITION_KD.get(),
+        new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
     xController.setTolerance(POSITION_TOLERANCE);
 
-    ProfiledPIDController yController =
-        new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(), POSITION_KD.get(),
-            new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
+    ProfiledPIDController yController = new ProfiledPIDController(POSITION_KP.get(), POSITION_KI.get(),
+        POSITION_KD.get(),
+        new TrapezoidProfile.Constraints(POSITION_MAX_VELOCITY, POSITION_MAX_ACCELERATION));
     yController.setTolerance(POSITION_TOLERANCE);
 
     return Commands.run(() -> {
@@ -524,8 +510,7 @@ public class DriveCommands {
       // (slowModeSupplier.getAsBoolean() ? SLOW_MODE_MULTIPLIER : 1.0);
 
       // Get linear velocity
-      Translation2d linearVelocity =
-          getLinearVelocityFromJoysticks(-xSupplier.getAsDouble(), -ySupplier.getAsDouble());
+      Translation2d linearVelocity = getLinearVelocityFromJoysticks(-xSupplier.getAsDouble(), -ySupplier.getAsDouble());
 
       // Apply rotation deadband
       double omega = MathUtil.applyDeadband(omegaSupplier.getAsDouble(), DEADBAND);
@@ -561,7 +546,6 @@ public class DriveCommands {
             omega = angleController.calculate(drive.getRotation().getRadians(),
                 closestPose.getRotation().getRadians());
           }
-
 
         }
       }
