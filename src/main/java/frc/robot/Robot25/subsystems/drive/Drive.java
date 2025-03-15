6ddd15521ade6f3885c999ac54d9e11501991ba7
SeanCh7 <153043733+Ebot404@users.noreply.github.com>
2025-03-15 08:19:01 -0500

    Revert "Squashed commit of the following:"
    This reverts commit 34657996a748d7e5188da9751245169139e0638f.

--- src/main/java/frc/robot/Robot25/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/Robot25/subsystems/drive/Drive.java
@@ -15,7 +15,6 @@ package frc.robot.Robot25.subsystems.drive;
 
 import static edu.wpi.first.units.Units.*;
 
-import com.ctre.phoenix6.swerve.SwerveDrivetrain;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.config.ModuleConfig;
 import com.pathplanner.lib.config.RobotConfig;
@@ -61,8 +60,6 @@ import org.ironmaple.simulation.drivesims.configs.DriveTrainSimulationConfig;
 import org.ironmaple.simulation.drivesims.configs.SwerveModuleSimulationConfig;
 import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
-
-// class test extends SwerveDrivetrain<> {}
 
 public class Drive extends SubsystemBase implements VisionConsumer {
 
@@ -211,16 +208,6 @@ public class Drive extends SubsystemBase implements VisionConsumer {
 
     // Update gyro alert
     gyroDisconnectedAlert.set(!gyroInputs.connected && SimConstants.CURRENT_MODE != Mode.SIM);
-  }
-
-  public void runVelocityFieldRelative(ChassisSpeeds speeds) {
-    var alliance = DriverStation.getAlliance().orElse(Alliance.Blue);
-    Rotation2d offset = switch (alliance) {
-      case Blue -> Rotation2d.kZero;
-      case Red -> Rotation2d.k180deg;
-    };
-    
-    runVelocity(ChassisSpeeds.fromFieldRelativeSpeeds(speeds, getRotation().plus(offset)));
   }
 
   /**
