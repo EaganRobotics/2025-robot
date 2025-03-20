43fcafd7a820219a51ec259469b0c2161f0bb5cb
notGriffin <krima724@gmail.com>
2025-03-20 10:32:22 -0500

    Update Elevator.java

--- src/main/java/frc/robot/Robot25/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/Robot25/subsystems/elevator/Elevator.java
@@ -63,7 +63,7 @@ public class Elevator extends SubsystemBase {
 
     minHeight(MIN_HEIGHT), Intake(Inches.of(16.4 + 3.5)), L1(Inches.of(18 + 14)), L2(
         Inches.of(31.9 + 7)), L3(
-            Inches.of(47.6 + 7)), L4(Inches.of(72 + 6.25)), Algae(Inches.of(69));
+            Inches.of(47.6 + 7)), L4(Inches.of(72 + 6.25)), Algae(Inches.of(80));
 
     private final Distance height;
 
