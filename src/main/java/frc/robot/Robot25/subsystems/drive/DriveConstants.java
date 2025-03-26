c3d72592a8b5d6a1b48be00ae681161655eb7bc6
notGriffin <123132870+notJithin@users.noreply.github.com>
2025-03-25 20:33:25 -0400

    Update at 'Tue Mar 25 20:33:25 EDT 2025'

--- src/main/java/frc/robot/Robot25/subsystems/drive/DriveConstants.java
+++ src/main/java/frc/robot/Robot25/subsystems/drive/DriveConstants.java
@@ -34,7 +34,7 @@ public class DriveConstants {
 
   // PID Gains for PathPlanner
   public static final PIDConstants PP_TRANSLATION_GAINS = new PIDConstants(1.3, 0.0, 0.01);
-  public static final PIDConstants PP_ROTATION_GAINS = new PIDConstants(2.6, 0.0, 0.3);
+  public static final PIDConstants PP_ROTATION_GAINS = new PIDConstants(2.6, 0.0, 0.1);
 
   /* These Gains constants only affect real hardware */
   public static class Real {
